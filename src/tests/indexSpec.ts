0d272bbbd9a0d97a53cd2d66f8a6e7a748b0e711
Mahmoud Ahmad <mahmoudcyyki@gmail.com>
2022-02-14 01:59:09 +0200

    Finish setting up linting and testing

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,5 +1,5 @@
-describe("An Example suite", () => {
-  it("should be ok", () => {
-    expect(true).toBe(true)
-  })
-})
+describe('An Example suite', () => {
+  it('should be ok', () => {
+    expect(true).toBe(true);
+  });
+});
